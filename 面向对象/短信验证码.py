04b14ab36989a5126f64801ed10ee8b0e91ff25a
donghao <1417766861@qq.com>
2018-08-17 22:05:32 +0800

    互亿无线短信发送成功！

---
+++ 面向对象/短信验证码.py
@@ -0,0 +1,29 @@
+import http.client
+import urllib
+
+host = "106.ihuyi.com"
+sms_send_uri = "/webservice/sms.php?method=Submit"
+
+# 查看用户名 登录用户中心->验证码通知短信>产品总览->API接口信息->APIID
+account = "C32656730"
+# 查看密码 登录用户中心->验证码通知短信>产品总览->API接口信息->APIKEY
+password = "5d630095914b477bc2a984b07d5abc8b"
+
+
+def send_sms(text, mobile):
+    params = urllib.parse.urlencode(
+        {'account': account, 'password': password, 'content': text, 'mobile': mobile, 'format': 'json'})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = http.client .HTTPConnection(host, port=80, timeout=30)
+    conn.request("POST", sms_send_uri, params, headers)
+    response = conn.getresponse()
+    response_str = response.read()
+    conn.close()
+    return response_str
+
+
+if __name__ == '__main__':
+    mobile = "17625567763"
+    text = "您的验证码是：121254。请不要把验证码泄露给其他人。"
+
+    print(send_sms(text, mobile))
